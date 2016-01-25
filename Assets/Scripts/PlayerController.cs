cbf043350da82706dcca0d98e4e0063a219dd708
Ritchie Hofmann <rhofma2@lsu.edu>
2016-01-24 18:21:20 -0600

    Add Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float movementSpeed;
+	public float rotateSpeed;
+
+	private Animation anim;
+
+	void Start () {
+		anim = GetComponent<Animation> ();
+	}
+
+	void Update () {
+		if (Input.anyKey) {
+			anim.Play ("Run");
+			if (Input.GetKey (KeyCode.W)) 
+				transform.Translate (Vector3.forward * movementSpeed * Time.deltaTime);
+			if (Input.GetKey (KeyCode.A)) 
+				transform.Translate (Vector3.left * movementSpeed * Time.deltaTime);
+			if (Input.GetKey (KeyCode.S)) 
+				transform.Translate (Vector3.back * movementSpeed * Time.deltaTime);
+			if (Input.GetKey (KeyCode.D)) 
+				transform.Translate (Vector3.right * movementSpeed * Time.deltaTime);
+			/*if (Input.GetKey (KeyCode.UpArrow)) 
+				transform.Rotate(Vector3.forward*rotateSpeed*Time.deltaTime);*/
+			if (Input.GetKey (KeyCode.LeftArrow)) 
+				transform.Rotate (Vector3.down * rotateSpeed * Time.deltaTime);
+			/*if (Input.GetKey (KeyCode.DownArrow)) 
+				transform.Rotate(Vector3.back*rotateSpeed*Time.deltaTime);*/
+			if (Input.GetKey (KeyCode.RightArrow)) 
+				transform.Rotate (Vector3.up * rotateSpeed * Time.deltaTime);
+		} else {
+			anim.Play ("idle");
+		}
+	}
+}
